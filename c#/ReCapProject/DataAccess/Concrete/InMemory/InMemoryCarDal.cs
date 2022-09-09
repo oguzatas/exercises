64d3263e62ae8ee1e9ae61a9fbfed565065e4c1c
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2022-09-09 19:41:35 +0300

    initial commit

---
+++ c#/ReCapProject/DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI.Concrete.InMemory
+{
+    internal class InMemoryCarDal
+    {
+    }
+}
