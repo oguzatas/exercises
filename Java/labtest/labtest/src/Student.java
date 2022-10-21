79fabe4a95b99fd1f3a6cb95ffc3acb206dbd14e
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2022-10-21 11:30:05 +0300

    changes

---
+++ Java/labtest/labtest/src/Student.java
@@ -0,0 +1,38 @@
+public class Student {
+    private int quiz1;
+    private int quiz2;
+    private int midterm;
+    private int fiinal;
+
+    public int getQuiz1() {
+        return quiz1;
+    }
+
+    public void setQuiz1(int quiz1) {
+        this.quiz1 = quiz1;
+    }
+
+    public int getQuiz2() {
+        return quiz2;
+    }
+
+    public void setQuiz2(int quiz2) {
+        this.quiz2 = quiz2;
+    }
+
+    public int getMidterm() {
+        return midterm;
+    }
+
+    public void setMidterm(int midterm) {
+        this.midterm = midterm;
+    }
+
+    public int getFiinal() {
+        return fiinal;
+    }
+
+    public void setFiinal(int fiinal) {
+        this.fiinal = fiinal;
+    }
+}
