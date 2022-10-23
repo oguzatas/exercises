c32fbb1f472a28ffa09243c1cd68e89ade3dda43
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2022-10-23 16:17:15 +0300

    Inceptors

---
+++ c#/Classic Architecture/Classic/Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+//One of the most important parts of the project. Top level programming example. Study It. Work On It.
+namespace Core.Utilities.Interceptors
+{
+
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+           // classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); code for logging.
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
---
+++ c#/Classic Architecture/Classic/Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Castle.DynamicProxy;
+using IInterceptor = Castle.DynamicProxy.IInterceptor;
+
+//One of the most important parts of the project. Top level programming example. Study It. Work On It.
+namespace Core.Utilities.Interceptors
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
+    {
+        public int Priority { get; set; }
+
+        public virtual void Intercept(IInvocation invocation)
+        {
+
+        }
+    }
+}
