f340ae79af194211b12a4d4c3a058b974c2c03ed
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2023-02-26 13:10:11 +0300

    aspects, transaction and performance

--- c#/Classic Architecture/Classic/Business/Concrete/ProductManager.cs
+++ c#/Classic Architecture/Classic/Business/Concrete/ProductManager.cs
@@ -3,6 +3,8 @@ using Business.BusinessAspects.Autofac;
 using Business.CCS;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
+using Core.Aspects.Autofac.Transaction;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Business;
@@ -37,13 +39,14 @@ namespace Business.Concrete
 
             _categoryService = categoryService;
         }
+        //inmemory cache
         
-
         public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return new SuccessDataResult<List<ProductDetailDto>>(_productDal.GetProductDetails());
         }
 
+        [CacheAspect] // key,value
         public IDataResult<List<Product>> GetAll()
         {
             //Codes
@@ -70,10 +73,12 @@ namespace Business.Concrete
             return new SuccessDataResult<List<Product>>
                 (_productDal.GetAll(p => p.UnitPrice>=min && p.UnitPrice<=max));
         }
+        //If cache hasn't been managed properly, it may not match with the database
 
         //Claim => yetki  
         [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Add(Product product)
         {
 
@@ -128,12 +133,14 @@ namespace Business.Concrete
 
         }
 
+        [CacheAspect]
         public IDataResult<Product> GetById(int productId)
         {
             return new SuccessDataResult<Product>(_productDal.Get(p => p.ProductId == productId));
         }
 
         [ValidationAspect(typeof(ProductValidator))]
+        [CacheRemoveAspect("IProductService.Get")]
         public IResult Update(Product product)
         {
             throw new NotImplementedException();
@@ -167,6 +174,12 @@ namespace Business.Concrete
                 return new ErrorResult(Messages.CategoryLimitExceded);
             }
             return new SuccessResult();
+        }
+
+        [TransactionScopeAspect]
+        public IResult AddTransactionalTest(Product product)
+        {
+            throw new NotImplementedException();
         }
     }
 
--- c#/Classic Architecture/Classic/Core/Utilities/Interceptors/AspectInterceptorSelector.cs
+++ c#/Classic Architecture/Classic/Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -1,4 +1,5 @@
 ﻿using Castle.DynamicProxy;
+using Core.Aspects.Autofac.Performance;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using System;
 using System.Collections.Generic;
@@ -11,7 +12,7 @@ using IInterceptor = Castle.DynamicProxy.IInterceptor;
 //One of the most important parts of the project. Top level programming example. Study It. Work On It.
 namespace Core.Utilities.Interceptors
 {
-
+    // add aspects here if you want to use them on all methods 
     public class AspectInterceptorSelector : IInterceptorSelector
     {
         public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
@@ -21,8 +22,8 @@ namespace Core.Utilities.Interceptors
             var methodAttributes = type.GetMethod(method.Name)
                 .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
             classAttributes.AddRange(methodAttributes);
-           // classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); code for logging.
-
+            //   classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger))); code for logging.
+            //   classAttributes.Add(new PerformanceAspect(typeof(PerformanceAspect)));
             return classAttributes.OrderBy(x => x.Priority).ToArray();
         }
     }
