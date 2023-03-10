ea0a15d3c774551dbb64b6f471030c5aa2ab3217
Oğuzhan Ataş <oguzhanatas37@gmail.com>
2023-03-10 16:43:47 +0300

    refactor

--- frontend/Angular/northwind/src/app/components/product/product.component.ts
+++ frontend/Angular/northwind/src/app/components/product/product.component.ts
@@ -1,30 +1,37 @@
 import { ProductService } from './../../services/product.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
   products: Product[] = [];
   dataLoaded = false;
+  destroy$ = new Subject();
 
 
   constructor(private productService: ProductService, private activatedRoute: ActivatedRoute) { } //activatedroute is a built in service in angular
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe(params => {
-      if (params["categoryId"]) {
-        this.getProductsByCategory(params["categoryId"])
+    this.activatedRoute.queryParams.pipe(takeUntil(this.destroy$)).subscribe(params => {
+      if (params["id"]) {
+        this.getProductsByCategory(params["id"]);
       }
       else {
         this.getProducts();
       }
     })
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(1);
+    this.destroy$.complete();
   }
 
   getProducts() {
